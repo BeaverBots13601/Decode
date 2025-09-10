d24f7dd699e582d9047df9d5b2a230b6b607b6c0
WHS Robotics <woodlandroboticsteam@woodlandschools.org>
2025-09-10 16:59:45 -0700

    Revert changes to Riser.kt to e7bf58

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Riser.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Riser.kt
@@ -1,40 +1,24 @@
 package org.firstinspires.ftc.teamcode.hardware
 
-import com.qualcomm.robotcore.hardware.DcMotorEx
-import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcontroller.teamcode.GamepadButtons
 import org.firstinspires.ftc.robotcontroller.teamcode.HardwareMechanismKt
 import org.firstinspires.ftc.robotcore.external.Telemetry
 
-class Riser(hardwareMap: HardwareMap, initData: InitData, telemetry: Telemetry) : HardwareMechanismKt() {
-    private val leftMotor = hardwareMap.get(DcMotorEx::class.java, "leftRiserMotor")
-    private val rightMotor = hardwareMap.get(DcMotorEx::class.java, "rightRiserMotor")
-
-    init {
-        rightMotor.direction = Direction.REVERSE
+class Riser(hardwareMap: HardwareMap, initData: InitData, private val telemetry: Telemetry) : HardwareMechanismKt() {
+    override fun start() {
+        TODO("Not yet implemented")
     }
-
-    override fun start() {}
 
     override fun run(data: RunData) {
-        if (data.currentGamepadOne.right_bumper) {
-            leftMotor.power = 0.5
-            rightMotor.power = 0.5
-        } else if (data.currentGamepadOne.left_bumper) {
-            leftMotor.power = -0.5
-            rightMotor.power = -0.5
-        } else {
-            leftMotor.power = 0.0
-            rightMotor.power = 0.0
-        }
+        TODO("Not yet implemented")
     }
 
-    override fun stop() {}
+    override fun stop() {
+        TODO("Not yet implemented")
+    }
 
-    override val usedButtons: Array<GamepadButtons> = arrayOf(
-        GamepadButtons.GP1_RIGHT_BUMPER,
-    )
+    override val usedButtons: Array<GamepadButtons> = emptyArray()
 
     companion object : HardwareMechanismSingletonManager<Riser>(::Riser)
 }
