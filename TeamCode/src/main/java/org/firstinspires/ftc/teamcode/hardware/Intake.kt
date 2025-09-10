e7bf58e3be1ab4d4a12c39445a39dedb1a7fd402
WHS Robotics <woodlandroboticsteam@woodlandschools.org>
2025-09-10 16:59:45 -0700

    Init mechanisms

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Intake.kt
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.hardware
+
+import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.robotcontroller.teamcode.GamepadButtons
+import org.firstinspires.ftc.robotcontroller.teamcode.HardwareMechanismKt
+import org.firstinspires.ftc.robotcore.external.Telemetry
+
+class Intake(hardwareMap: HardwareMap, initData: InitData, private val telemetry: Telemetry) : HardwareMechanismKt() {
+    override fun start() {
+        TODO("Not yet implemented")
+    }
+
+    override fun run(data: RunData) {
+        TODO("Not yet implemented")
+    }
+
+    override fun stop() {
+        TODO("Not yet implemented")
+    }
+
+    override val usedButtons: Array<GamepadButtons> = emptyArray()
+
+    companion object : HardwareMechanismSingletonManager<Intake>(::Intake)
+}
