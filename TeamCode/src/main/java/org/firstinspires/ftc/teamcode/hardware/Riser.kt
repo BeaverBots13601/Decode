de81230c8049ddac1e43daae3d94d29db950b3d2
WHS Robotics <woodlandroboticsteam@woodlandschools.org>
2025-10-13 17:22:04 -0700

    Init riser

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Riser.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Riser.kt
@@ -1,21 +1,30 @@
 package org.firstinspires.ftc.teamcode.hardware
 
+import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcontroller.teamcode.GamepadButtons
 import org.firstinspires.ftc.robotcontroller.teamcode.HardwareMechanismKt
 import org.firstinspires.ftc.robotcore.external.Telemetry
 
 class Riser(hardwareMap: HardwareMap, initData: InitData, private val telemetry: Telemetry) : HardwareMechanismKt() {
+    val leftRiserMotor = hardwareMap.dcMotor.get("LeftRiserMotor")
+    val rightRiserMotor = hardwareMap.dcMotor.get("RightRiserMotor")
+
     override fun start() {
-        TODO("Not yet implemented")
+        leftRiserMotor.power = 0.0
+        rightRiserMotor.power = 0.0
+
+        rightRiserMotor.direction = DcMotorSimple.Direction.REVERSE
     }
 
     override fun run(data: RunData) {
-        TODO("Not yet implemented")
+        leftRiserMotor.power = 1.0
+        rightRiserMotor.power = 1.0
     }
 
     override fun stop() {
-        TODO("Not yet implemented")
+        leftRiserMotor.power = 0.0
+        rightRiserMotor.power = 0.0
     }
 
     override val usedButtons: Array<GamepadButtons> = emptyArray()
